9b60eacf8be99c9d17b851b3daf65d4a6a47bf62
Sebastian Parada C <111198689+j-sebastian-pc@users.noreply.github.com>
2023-11-06 20:12:45 -0500

    Update vite.config.js
    cambio

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // ... otras configuraciones ...
   build: {
     rollupOptions: {
-      external: ['react-toastify'],
+      external: ['react-toastify', 'axios'], // Agregar 'axios' a la lista de externos
     },
   },
 });
